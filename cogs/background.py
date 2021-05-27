b75b113360b753eecb9758d7766ae96939adc16e
gerarar <gerardmartinp@gmail.com>
2021-05-26 21:25:07 -0400

    imported all background related tasks

---
+++ cogs/background.py
@@ -0,0 +1,339 @@
+import discord
+from discord.ext import commands
+import time
+import os
+import sys
+import datetime
+import asyncio
+import random
+import mysql.connector
+import inspect
+import mysqlConnection_local as sql # mysql file
+import traceback
+import pytz
+import threading
+import string
+from dotenv import load_dotenv
+
+load_dotenv() # adds environment variables to current environment
+
+"""
+	This cog is for commands dealing with background tasks/processes
+"""
+class Background(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.on_ready_status = True
+
+		self.log_channel_id = int(os.getenv("LOG_CHANNEL"))
+		self.reminders_dict = {
+			"Reminders_10Minutes":
+				{
+					"Warning_Msg": string.Template("***FINAL*** **$class_name HOMEWORK REMINDER:** `$class_obj` is due in less then **10 minutes**!! $class_role_mention")
+				},
+			"Reminders_3Hours":
+				{
+					"Warning_Msg": string.Template("**$class_name HOMEWORK REMINDER:** `$class_obj` is due in less then **3 hours**!! $class_role_mention")
+				},
+			"Reminders_24Hours":
+				{
+					"Warning_Msg": string.Template("**$class_name HOMEWORK REMINDER:** `$class_obj` is due in less then **3 hours**!! $class_role_mention")
+				}
+		}
+		self.class_sub_channel_id = 618210352341188618
+
+
+	"""
+		>> Helper function to delete a message after buf number of seconds
+	"""
+	async def delete_message(self, chnl, msg, buf: int = 3):
+		try:
+			await asyncio.sleep(buf)
+			await msg.delete()
+		except discord.errors.NotFound:
+			pass
+		return
+
+
+	"""
+		Helper fuction to udpate the serer stats of number of members, excluding bots, in the server/guild
+	"""
+	async def update_guild_member_count(self):
+		psu_discord = self.bot.get_guild(575004997327126551)
+
+		f = lambda x : x.bot == False
+
+		members = psu_discord.members
+		num_members = list(filter(f, members)) # remove bots from member list/count
+
+		for cat in psu_discord.categories:
+			if "server stats" in cat.name.lower():
+				await cat.channels[0].delete()
+
+				overwrite = discord.PermissionOverwrite()
+				overwrite.connect = False
+
+				perms = {
+					psu_discord.default_role: overwrite
+				}
+
+				VC = await cat.create_voice_channel(f"Total Members: {len(num_members)}", overwrites=perms)
+				break
+		print("Edited total member VC")
+
+
+	"""
+		Helper fuction to reorder the class channels by thier respective member counts
+	"""
+	async def reorder_channels(self):
+		BLACKLIST = ["Admin","GiveawayBot","Nadeko","Bots","Mod","dabBot","@everyone","Simple Poll","Groovy"]
+
+		psu_discord = self.bot.get_guild(575004997327126551)
+		raw_roles = psu_discord.roles
+
+		f = lambda r : r.name not in BLACKLIST
+
+		roles = list(filter(f, raw_roles))
+		roles.sort(key=lambda x: (len(x.members), x.name), reverse=True)
+
+		for index, role in enumerate(roles):
+			pos = len(raw_roles)-len(BLACKLIST)-index
+			if role.position == pos:
+				print("{} already in position {}".format(role.name, pos))
+				continue
+
+			print("Set {} to position {}".format(role.name, pos))
+			await role.edit(position=pos)
+			await asyncio.sleep(1)
+
+
+	"""
+		Listener event for on_member_remove
+		>>	called whenever a user leaves the guild/server whether be intentional or a ban
+		>>	https://discordpy.readthedocs.io/en/stable/api.html#discord.on_member_remove
+	"""
+	async def reminder_process(self):
+		mydb, my_cursor = sql.connect()
+
+		for table, value in self.reminders_dict:
+			my_cursor.execute("SELECT * FROM %s WHERE Datetimestamp < %s", (table, datetime.datetime.now(),))
+			d = my_cursor.fetchall()
+			print(f'{key}: {d}')
+			
+			if len(d) > 0:
+				for a in d:
+					my_cursor.execute("SELECT Class_role_id FROM Classes WHERE Class_name = %s", (a[1],))
+					obj = my_cursor.fetchall()
+
+					data = {
+						"class_name": a[1],
+						"class_obj": a[3],
+						"class_role_mention": {discord.utils.get(self.bot.get_guild(575004997327126551).roles, id=int(a[0])).mention}
+					}
+
+					# this will sub in data values into the $-placeholders by matching dictionary keys to placeholder names
+					m = await self.bot.get_channel(618818304936640533).send(value["Warning_Msg"].substitute(data))
+
+					await m.add_reaction(emoji="🖕")
+					my_cursor.execute("DELETE FROM %s WHERE ID = %s", (table, a[0],)) #delete from table
+					mydb.commit()
+
+		sql.close(mydb, my_cursor)
+		await asyncio.sleep(60)
+
+
+	async def my_background_task(self):
+
+		await self.bot.wait_until_ready()
+		while not self.bot.is_closed():
+
+			try:
+				await self.reminder_process()
+			except Exception:
+				pass
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+
+		if self.on_ready_status: # this should only run once, especially when the API disconnects and reconnects
+			self.on_ready_status = False
+
+			mydb, my_cursor = sql.connect()
+
+			my_cursor.execute("SELECT Class_name FROM Classes ORDER BY Class_name")
+			d = my_cursor.fetchall()
+			
+			arr = []
+			for i in d:
+				if i[0].split(" ")[0] not in [a[0] for a in arr]: # check if department is accounted for yet
+					arr.append([i[0].split(" ")[0], [i[0]]]) # append [dept alias, [class]]
+				else:
+					for inner in arr:
+						if i[0].split(" ")[0] == inner[0]:
+							inner_arr = inner[1]
+							inner_arr.append(i[0])
+							break
+
+			f = lambda x: len(x[1]) # returns length of nested list
+			arr.sort(key=f, reverse=True) # sort array by number of classes in each department/field from greatest to smallest
+
+			await self.bot.get_channel(self.class_sub_channel_id).purge(limit=10)
+			
+			est = pytz.timezone('US/Eastern')
+			em = discord.Embed(description = "**Join a class group chat and receive notifications by invoking one of the following command(s) below**:\n\n",
+								color=0x10D600, timestamp=datetime.datetime.now())
+
+			my_cursor.execute("SELECT Department_name, Department_alias FROM Departments")
+			d = my_cursor.fetchall()
+			# all_dept_names = [b[0] for b in d]
+
+			for field in arr:
+				text = ""
+				for classs in field[1]:
+					text += "{} - `!join {}`\n".format(classs, classs.split(" ")[1]) # join nested array together to string
+
+				for dept in d:
+					if field[0] == dept[1]:	
+						em.add_field(name="{} | {}".format(dept[0], dept[1]), 
+							value=text, inline=True)
+						break
+
+			await self.bot.get_channel(self.class_sub_channel_id).send(embed=em)
+
+			await self.update_guild_member_count()
+			await self.reorder_channels()
+
+			sql.close(mydb, my_cursor)
+
+			# bot.loop.create_task(await my_background_task())
+			await asyncio.gather(self.my_background_task())
+
+
+	"""
+		Listener event for on_message_delete
+		>>	called whenever a message is deleted
+		>>	https://discordpy.readthedocs.io/en/stable/api.html#discord.on_message_delete
+	"""
+	@commands.Cog.listener()
+	async def on_message_delete(self, msg):
+
+		# Check if message is not from bot and not a command 
+		if msg.author.id != self.bot.user.id and not any(msg.content.upper().startswith(s.upper()) for s in self.auto_delete_commands):
+			est = pytz.timezone('US/Eastern')
+			joined_date = msg.author.joined_at.astimezone(est).strftime('%a %b %d %Y %-I:%M%p')
+
+			em = discord.Embed( 
+				description=f"► Name: `{msg.author.name}#{msg.author.discriminator}` {msg.author.mention} [{msg.author.id}]\n► Joined Server On: **{joined_date}**\n► Message ID: {msg.id}", 
+				color=0xFF8B00, timestamp=datetime.datetime.now())
+
+			em.set_author(name = f"Message by {msg.author.name}#{msg.author.discriminator} deleted in #{msg.channel.name}", icon_url = msg.author.avatar_url)
+			em.add_field(name="Message Content", value=msg.content, inline=False)
+			
+			staff_log_channel = self.bot.get_channel(self.log_channel_id)
+			await staff_log_channel.send(embed=em)
+
+
+	"""
+		Listener event for on_raw_message_delete
+		>>	called whenever an uncached message is deleted
+		>>	a message is uncached is when a message is older than the bot's uptime
+		>>	https://discordpy.readthedocs.io/en/stable/api.html#discord.on_raw_message_delete
+	"""
+	@commands.Cog.listener()
+	async def on_raw_message_delete(self, payload):
+
+		# Check if message is uncached
+		if payload.cached_message == "" or payload.cached_message == None:
+			est = pytz.timezone('US/Eastern')
+			em = discord.Embed( 
+				description=f"► Message ID: {payload.message_id}", 
+				color=0xFF8B00, timestamp=datetime.datetime.now())
+
+			try:
+				em.set_author(name = f"Uncached Message deleted in #{self.bot.get_channel(payload.channel_id).name}")
+			except TypeError:
+				return
+			
+			staff_log_channel = self.bot.get_channel(self.log_channel_id)
+			await staff_log_channel.send(embed=em)	
+
+
+	"""
+		Listener event for on_message_edit
+		>>	called whenever a message is edited
+		>>	https://discordpy.readthedocs.io/en/stable/api.html#discord.on_message_edit
+	"""
+	@commands.Cog.listener()
+	async def on_message_edit(self, before, after):
+		if before.author.id != self.bot.user.id:
+			est = pytz.timezone('US/Eastern')
+			joined_date = before.author.joined_at.astimezone(est).strftime('%a %b %d %Y %-I:%M%p')
+
+			em = discord.Embed( 
+				description=f"► Name: `{before.author.name}#{before.author.discriminator}` {before.author.mention} [{before.author.id}]\n► Joined Server On: **{joined_date}**\n► Message ID: {before.id}", 
+				color=0x0076FA, timestamp=datetime.datetime.now())
+
+			em.set_author(name = f"Message in #{before.channel.name} edited by {before.author.name}#{before.author.discriminator}", icon_url = before.author.avatar_url)
+			em.add_field(name="Original Content", value=before.content, inline=True)
+			em.add_field(name="New Content", value=after.content, inline=True)
+
+			staff_log_channel = self.bot.get_channel(self.log_channel_id)
+			await staff_log_channel.send(embed=em)
+
+		# Counting channel check
+		if before.channel.id == 715963289494093845:
+			l_msg = await before.channel.history(limit=1).flatten()
+			print(before.id, l_msg[0].id)
+			if before.id == l_msg[0].id:
+				if "{0:b}".format(counting_number) not in after.content.split(" "):
+					await before.channel.send("Current number was edited.\n")
+					await before.channel.send("{0:b}".format(counting_number))
+
+
+	"""
+		Listener event for on_member_update
+		>>	called whenever a member updates their profile
+		>>	https://discordpy.readthedocs.io/en/stable/api.html#discord.on_member_update
+	"""
+	@commands.Cog.listener()
+	async def on_member_update(self, before, after):
+		if before.nick != after.nick:
+			est = pytz.timezone('US/Eastern')
+			joined_date = before.joined_at.astimezone(est).strftime('%a %b %d %Y %-I:%M%p')
+
+			em = discord.Embed( 
+				description=f"► Name: `{before.name}#{before.discriminator}` {before.mention} [{before.id}]\n► Joined Server On: **{joined_date}**", 
+				color=0xFFF25A, timestamp=datetime.datetime.now())
+
+			em.set_author(name = f"{before.name}#{before.discriminator}'s nickname was changed", icon_url = before.avatar_url)
+			em.add_field(name="Original Nickname", value=before.nick, inline=True)
+			em.add_field(name="New Nickname", value=after.nick, inline=True)
+
+			staff_log_channel = self.bot.get_channel(self.log_channel_id)
+			await staff_log_channel.send(embed=em)
+
+		if before.status != after.status:
+			est = pytz.timezone('US/Eastern')
+			joined_date = before.joined_at.astimezone(est).strftime('%a %b %d %Y %-I:%M%p')
+
+			em = discord.Embed( 
+				description=f"► Name: `{before.name}#{before.discriminator}` {before.mention} [{before.id}]\n► Joined Server On: **{joined_date}**", 
+				color=0xFFF25A, timestamp=datetime.datetime.now())
+
+			em.set_author(name = f"{before.name}#{before.discriminator}'s status was changed", icon_url = before.avatar_url)
+			em.add_field(name="Original Status", value=str(before.status), inline=True)
+			em.add_field(name="New Status", value=str(after.status), inline=True)
+
+			staff_log_channel = self.bot.get_channel(self.log_channel_id)
+			await staff_log_channel.send(embed=em)
+
+
+def setup(bot):
+	"""
+		>> https://discordpy.readthedocs.io/en/latest/ext/commands/cogs.html
+		An extension must have a global function, setup 
+			defined as the entry point on what to do when 
+			the extension is loaded. 
+		This entry point must have a single argument, the bot.
+	"""
+	bot.add_cog(Background(bot)) # add cog/Class by passing in instance
